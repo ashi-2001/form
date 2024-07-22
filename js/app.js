78c3a05efcfd30e8ca290eed4451205ab3f97abf
ashi-2001 <75829507+ashi-2001@users.noreply.github.com>
2024-07-22 17:51:26 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var student = {
+    firstname : "ashi"
+}
+console.log(student.firstname)
+var s= JSON.stringify(student)
+console.log(s)
+
+var f_l=student.firstname.charAt(0);
+f_l=JSON.stringify(f_l)
+console.log(f_l)
