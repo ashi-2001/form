c5f3ae31076f17110cb60718856bb25737470b86
ashi-2001 <75829507+ashi-2001@users.noreply.github.com>
2024-07-23 11:56:27 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
-var student = {
-    firstname : "ashi"
-}
-console.log(student.firstname)
-var s= JSON.stringify(student)
-console.log(s)
-
-var f_l=student.firstname.charAt(0);
-f_l=JSON.stringify(f_l)
-console.log(f_l)
+function handleSubmit(event) {
+    event.preventDefault();
+  
+    const data = new FormData(event.target);
+    const result = {}
+    result.firstName=data.get("firstName");
+    console.log({})
+    
+  }
+  
+  const form = document.querySelector('form');
+  form.addEventListener('submit', handleSubmit);
